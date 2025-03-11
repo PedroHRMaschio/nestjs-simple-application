431b30a41b30b622dbcfc17883b1ff9c16cdc1ac
PedroHRMaschio <pedrohenriquermaschio@gmail.com>
2025-03-10 21:55:45 -0300

    feat: add new routes

--- lesson01/src/users/users.controller.ts
+++ lesson01/src/users/users.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // /users
+export class UsersController {
+  /*
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+    */
+
+  @Get() // GET /users
+  findAll() {
+    return [];
+  }
+
+  @Get('interns') // GET /users/interns
+  findAllInterns() {
+    return [];
+  }
+  // The order of the route decorators is important!
+  // if the route interns comes after the route :id, the interns route will never be reached.
+  // The application will think the interns route is an id parameter.
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  remove(@Param('id') id: string) {
+    return { id };
+  }
+}
