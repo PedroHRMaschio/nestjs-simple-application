4f75b8f4e0c683593b24ffe513a218503d834b02
PedroHRMaschio <pedrohenriquermaschio@gmail.com>
2025-03-11 00:05:05 -0300

    fix: Query import

--- lesson01/src/employees/employees.controller.ts
+++ lesson01/src/employees/employees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
 
